8cd9bc123110caa9af1012d8a77e8e6534f6de85
Hangukkid <matthew.won@mail.utoronto.ca>
2020-09-30 19:04:42 -0400

    Adding uoft email functionality to the index page

--- ch4.py
+++ ch4.py
@@ -5,11 +5,17 @@ from datetime import datetime
 
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, ValidationError
 
 class NameForm(Form):
     name = StringField("What is your name?", validators=[Required()])
+    email = StringField("What is your UofT Email address?", validators=[Required()])
     submit = SubmitField("Submit")
+
+    def validate_email(form, field):
+        if (field.data.find('@') == -1):
+            raise ValidationError("Not a valid email address. Please include an '@'.")
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -21,11 +27,20 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        email = form.email.data
+        utoronto = email.find('utoronto')
+
+        # inserting email logic
+        if (utoronto == -1 or email.find('@') > utoronto):
+            session['email'] = "Please use your UofT email"
+        else:
+            session['email'] = "Your UofT email is " + email
+
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
             session['name'] = form.name.data
             return redirect(url_for('index'))
-    return render_template('index_ch4.html', form = form, name = session.get('name'))
+    return render_template('index_ch4.html', form = form, name = session.get('name'), email = session.get('email'))
 
 if __name__ == '__main__':
     app.run(debug=True)
